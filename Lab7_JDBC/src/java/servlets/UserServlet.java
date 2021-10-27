a43a6cba11acb54fed33a4d941bb413ad261a510
849533 <849533@SAIT227692IT.ACDM.DS.SAIT.CA>
2021-10-27 15:00:25 -0600

    UserServlet.java doGet removed all the unneeded code from doGet such as try catch blocks

--- Lab7_JDBC/src/java/servlets/UserServlet.java
+++ Lab7_JDBC/src/java/servlets/UserServlet.java
@@ -2,11 +2,13 @@ package servlets;
 
 import com.sun.istack.internal.logging.Logger;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import services.UserService;
 
 public class UserServlet extends HttpServlet {
 
@@ -16,47 +18,8 @@ public class UserServlet extends HttpServlet {
         
         UserService us = new UserService();
         
-        try {
-            HTTPSession session = request.getSession();
-            String email = (String) session.getAttribute("add_email");
-            List<User> users = us.getAll(email);
-            request.setAttribute("users", users);
-        } catch (Exception ex) {
-            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
-            request.setAttribute("message", "error");
-        }
-        
-        try {
-            HTTPSession session = request.getSession();
-            String firstName = (String) session.getAttribute("add_first_name");
-            List<User> users = us.getAll(firstName);
-            request.setAttribute("users", users);
-        } catch (Exception ex) {
-            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
-            request.setAttribute("message", "error");
-        }
-        
-        try {
-            HTTPSession session = request.getSession();
-            String lastName = (String) session.getAttribute("add_last_name");
-            List<User> users = us.getAll(lastName);
-            request.setAttribute("users", users);
-        } catch (Exception ex) {
-            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
-            request.setAttribute("message", "error");
-        }
-        
-        String action = request.getParameter("action");
-        if (action != null && action.equals("view")) {
-            try {
-                // need to double check the user id part here based on the completed JSP
-                int id = Integer.parseInt(request.getParameter("userId"));
-                User user = us.get(id);
-                request.setAttribute("selectedUser", user);
-            } catch (Exception ex) {
-                Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+        List<User> users = us.getAll();
+        request.setAttribute("users", users);
         
         getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
         return;
