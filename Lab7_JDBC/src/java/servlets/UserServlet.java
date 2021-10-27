66723703116ed9c16ef2eb16b3cfe9fe0f328674
849533 <849533@SAIT227692IT.ACDM.DS.SAIT.CA>
2021-10-27 14:40:57 -0600

    UserServlet.java test doGet test to see if people can work in same file at once

--- Lab7_JDBC/src/java/servlets/UserServlet.java
+++ Lab7_JDBC/src/java/servlets/UserServlet.java
@@ -1,6 +1,8 @@
 package servlets;
 
+import com.sun.istack.internal.logging.Logger;
 import java.io.IOException;
+import java.util.logging.Level;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +13,51 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        UserService us = new UserService();
+        
+        try {
+            HTTPSession session = request.getSession();
+            String email = (String) session.getAttribute("add_email");
+            List<User> users = us.getAll(email);
+            request.setAttribute("users", users);
+        } catch (Exception ex) {
+            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+            request.setAttribute("message", "error");
+        }
+        
+        try {
+            HTTPSession session = request.getSession();
+            String firstName = (String) session.getAttribute("add_first_name");
+            List<User> users = us.getAll(firstName);
+            request.setAttribute("users", users);
+        } catch (Exception ex) {
+            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+            request.setAttribute("message", "error");
+        }
+        
+                try {
+            HTTPSession session = request.getSession();
+            String lastName = (String) session.getAttribute("add_last_name");
+            List<User> users = us.getAll(lastName);
+            request.setAttribute("users", users);
+        } catch (Exception ex) {
+            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+            request.setAttribute("message", "error");
+        }
+        
+        String action = request.getParameter("action");
+        if (action != null && action.equals("view")) {
+            try {
+                // need to double check the user id part here based on the completed JSP
+                int id = Integer.parseInt(request.getParameter("userId"));
+                User user = us.get(id);
+                request.setAttribute("selectedUser", user);
+            } catch (Exception ex) {
+                Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
         getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
         return;
     }
