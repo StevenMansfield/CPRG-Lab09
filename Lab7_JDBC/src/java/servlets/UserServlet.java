e8e617f766613005addf9a2cfd9f1d4d5aeb7950
DNBarringer <davidnbarringer@gmail.com>
2021-10-27 14:44:40 -0600

    Created variables in models based on userdb.sql

--- Lab7_JDBC/src/java/servlets/UserServlet.java
+++ Lab7_JDBC/src/java/servlets/UserServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import services.UserService;
 
 public class UserServlet extends HttpServlet {
 
@@ -18,5 +19,19 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String myAction = request.getParameter("action");
+        String email = request.getParameter("add_email");
+        String firstName = request.getParameter("add_first_name");
+        String lastName = request.getParameter("add_last_name");
+        String password = request.getParameter("add_password");
+        String userType = request.getParameter("add_user_type");
+        UserService userService = new UserService();
+
+        if (myAction != null && myAction.equals("add")) {
+            
+        }
+
+        getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
+        return;
     }
 }
